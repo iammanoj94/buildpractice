e3243377d5903347ff64e9176ca169e6295bc50f
manoj <iammanoj94@gmail.com>
2019-03-19 11:46:39 +0530

    file corrected

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ import java.lang.*;
 class HelloWorld{
 	public static void main(String args[]){
 	
-		System.out.println("Hello Manoj Welcome to java");}
+		System.out.println("Hello Manoj Welcome to java");
 	}
 }
